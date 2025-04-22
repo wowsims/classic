7aa7bdc451bffa6cf1ed46b3921e0994c8a16cd3
Zephryl87 <46359763+Zephryl87@users.noreply.github.com>
2025-04-22 07:23:54 +0200

    Update and rename _items.go to items.go
    Not sure, if this is needed at all

---
+++ sim/priest/items.go
@@ -0,0 +1,27 @@
+package priest
+
+import (
+	"time"
+
+	"github.com/wowsims/classic/sim/core"
+)
+
+const (
+	// Keep these ordered by ID
+	AtieshPriest	= 22631
+)
+
+func init() {
+	core.AddEffectsToTest = false
+
+	// Keep these ordered by name
+
+	// https://www.wowhead.com/classic/item=22631/atiesh-greatstaff-of-the-guardian
+	core.NewItemEffect(AtieshPriest, func(agent core.Agent) {
+		character := agent.GetCharacter()
+		aura := core.AtieshHealingEffect(&character.Unit)
+		character.ItemSwap.RegisterProc(AtieshPriest, aura)
+	})
+	
+	core.AddEffectsToTest = true
+}
