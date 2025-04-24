8cae17ef3346ee18551a98b37e39845d00f5c113
Zephryl87 <46359763+Zephryl87@users.noreply.github.com>
2025-04-24 05:10:41 +0200

    Update presets.ts

--- ui/shadow_priest/presets.ts
+++ ui/shadow_priest/presets.ts
@@ -24,12 +24,18 @@ import APLShadowUDJSON from './apls/shadow_ud.apl.json';
 import APLShadowJSON from './apls/shadow.apl.json';
 import APLDiscJSON from './apls/disc.apl.json';
 import P0BISGear from './gear_sets/p0.bis.gear.json';
-import P1Shadow from './gear_sets/p1.shadow.json';
-import P2Shadow from './gear_sets/p2.shadow.json';
-import P3Shadow from './gear_sets/p3.shadow.json';
-import P4Shadow from './gear_sets/p4.shadow.json';
-import P5Shadow from './gear_sets/p5.shadow.json';
-import P6Shadow from './gear_sets/p6.shadow.json';
+import P1Shadow from './gear_sets/shadow.p1.json';
+import P2Shadow from './gear_sets/shadow.p2.json';
+import P3Shadow from './gear_sets/shadow.p3.json';
+import P4Shadow from './gear_sets/shadow.p4.json';
+import P5Shadow from './gear_sets/shadow.p5.json';
+import P6Shadow from './gear_sets/shadow.p6.json';
+import P1Disc from './gear_sets/disc.p1.json';
+import P2Disc from './gear_sets/disc.p2.json';
+import P3Disc from './gear_sets/disc.p3.json';
+import P4Disc from './gear_sets/disc.p4.json';
+import P5Disc from './gear_sets/disc.p5.json';
+import P6Disc from './gear_sets/disc.p6.json';
 
 // Preset options for this spec.
 // Eventually we will import these values for the raid sim too, so its good to
@@ -46,14 +52,20 @@ export const GearP3Shadow = PresetUtils.makePresetGear('P3 Shadow', P3Shadow);
 export const GearP4Shadow = PresetUtils.makePresetGear('P4 Shadow', P4Shadow);
 export const GearP5Shadow = PresetUtils.makePresetGear('P5 Shadow', P5Shadow);
 export const GearP6Shadow = PresetUtils.makePresetGear('P6 Shadow', P6Shadow);
+export const GearP1Disc = PresetUtils.makePresetGear('P1 Disc', P1Disc);
+export const GearP2Disc = PresetUtils.makePresetGear('P2 Disc', P2Disc);
+export const GearP3Disc = PresetUtils.makePresetGear('P3 Disc', P3Disc);
+export const GearP4Disc = PresetUtils.makePresetGear('P4 Disc', P4Disc);
+export const GearP5Disc = PresetUtils.makePresetGear('P5 Disc', P5Disc);
+export const GearP6Disc = PresetUtils.makePresetGear('P6 Disc', P6Disc);
 
 export const GearPresets = {
-	[Phase.Phase1]: [GearP0BIS, GearP1Shadow],
-	[Phase.Phase2]: [GearP2Shadow],
-	[Phase.Phase3]: [GearP3Shadow],
-	[Phase.Phase4]: [GearP4Shadow],
-	[Phase.Phase5]: [GearP5Shadow],
-	[Phase.Phase6]: [GearP6Shadow],
+	[Phase.Phase1]: [GearP0BIS, GearP1Shadow,GearP1Disc],
+	[Phase.Phase2]: [GearP2Shadow,GearP2Disc],
+	[Phase.Phase3]: [GearP3Shadow,GearP3Disc],
+	[Phase.Phase4]: [GearP4Shadow,GearP4Disc],
+	[Phase.Phase5]: [GearP5Shadow,GearP5Disc],
+	[Phase.Phase6]: [GearP6Shadow,GearP6Disc],
 };
 
 export const DefaultGear = GearP0BIS;
