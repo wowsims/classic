575ab975977433ee1a9eb514af0d308f7db94ace
Kayla Glick <kaylaglick12@gmail.com>
2025-10-12 11:34:53 -0400

    insect swarm can't crit

--- sim/druid/insect_swarm.go
+++ sim/druid/insect_swarm.go
@@ -75,7 +75,7 @@ func (druid *Druid) registerInsectSwarmSpell() {
 						}
 					},
 					OnTick: func(sim *core.Simulation, target *core.Unit, dot *core.Dot) {
-						dot.CalcAndDealPeriodicSnapshotDamage(sim, target, dot.OutcomeSnapshotCrit)
+						dot.CalcAndDealPeriodicSnapshotDamage(sim, target, dot.OutcomeTick)
 					},
 				},
 
